3efa6e5cc851f134317908bc8fdfdc9187f6319b
guskoro <guskoro@gmail.com>
2022-05-11 08:34:24 +0700

    Initial Commit

---
+++ rumahku/rumahkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  rumahkuApp.swift
+//  rumahku
+//
+//  Created by Guskoro Pradipta on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct rumahkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
